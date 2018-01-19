ad5478982dc8e3b2bd1f23d39fa8dcef747cdb32
Philip Kirkbride <phk@2klic.com>
2018-01-19 08:08:37 -0500

    compile to es5, not tested yet

--- src/FacebookApiException.js
+++ src/FacebookApiException.js
@@ -1,13 +1,29 @@
 'use strict';
 
-export default function FacebookApiException(res) {
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _create = require('babel-runtime/core-js/object/create');
+
+var _create2 = _interopRequireDefault(_create);
+
+var _stringify = require('babel-runtime/core-js/json/stringify');
+
+var _stringify2 = _interopRequireDefault(_stringify);
+
+exports.default = FacebookApiException;
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function FacebookApiException(res) {
 	this.name = 'FacebookApiException';
-	this.message = JSON.stringify(res || {});
+	this.message = (0, _stringify2.default)(res || {});
 	this.response = res;
 	Error.captureStackTrace(this, this.constructor.name);
 }
 
-FacebookApiException.prototype = Object.create(Error.prototype, {
+FacebookApiException.prototype = (0, _create2.default)(Error.prototype, {
 	constructor: {
 		value: FacebookApiException,
 		enumerable: false,
